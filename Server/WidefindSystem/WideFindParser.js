6b05863afe72087ff271df10c245712d365a4c88
PandaFood <jonathan.brorsson@gmail.com>
2019-01-30 14:32:56 +0100

    added callbacks to functions. Added comments

---
+++ Server/WidefindSystem/WideFindParser.js
@@ -0,0 +1,38 @@
+/**
+ * @fileOverview Class for parsing WideFind data to standardized json format
+ */
+
+
+/**
+ *     Outputs standardized JSON format
+ *  @param data Data to parse
+ */
+module.exports.jsonify = function(data){
+
+    var json = {
+        version:1,
+        action:0,
+        entities:[]
+    };
+
+    json.entities.push({
+        id:data.id,
+        type:"human",
+        data:{
+        position:{
+            x: data.pos[0],
+            y: data.pos[2],
+            z: data.pos[1]
+        },
+        velocity:{
+            x: data.vel[0],
+            y: data.vel[2],
+            z: data.vel[1]
+        },
+        battery: data.bat,
+        signal: data.rssi
+        }
+    });
+
+    return JSON.stringify(json);
+}
