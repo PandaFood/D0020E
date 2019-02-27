eb8e16d080461d21b3d901247eaef5380ed6b298
mikala-3 <36886960+mikala-3@users.noreply.github.com>
2019-02-27 14:53:39 +0100

    Created config

---
+++ Server/config.js
@@ -0,0 +1,29 @@
+/* CONFIG */
+var config = {
+    database = {
+        tunnel={
+            username:'root',
+            password:'vandrandelurar',
+            host:'130.240.207.20',
+            port:51315,
+            dstHost:'127.0.0.1',
+            dstPort:27017,
+            localHost:'127.0.0.1',
+            localPort: 27017
+          },
+        
+          numberunits: 5, //number of units logged
+          delay: 100,    //in ms delay per new data insert
+          spooflogged: 20,
+        
+          numberlogged: 20,
+          timeout: 1, //In minutes
+    },
+    rethink={
+        Host = '35.180.30.36',
+        Port = 28016,
+        User = 'admin',
+        Pass = 'UnlikelySnuggleBuild'
+    }
+};
+
